fa6820becbe7e8d9a3f863b5473ec25c8aeb3bd6
Pavel Lang <langpavel@phpskelet.org>
2026-03-02 12:38:59 +0100

    feat(films): add Films page and FilmCards component

---
+++ app/films/page.tsx
@@ -0,0 +1,10 @@
+import FilmCards from "@/components/films/FilmCards";
+
+export default function Films() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold m-4">Films</h1>
+      <FilmCards />
+    </main>
+  );
+}
